f90e9fd7a4892838efc9cbed5be8f2f3cd9be95a
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-15 00:09:49 +0530

    logging object to console.

--- app-object.js
+++ app-object.js
@@ -1,2 +1,4 @@
-const aObject = { id: 1, username: "sandeep" };
+// Object are key-value pairs and note reference type.
+const aObject = { id: 1, username: "sandeep" }; // here aObject is pointing to the address in the memory. where it stores the data.
 console.log(aObject);
+console.dir(aObject); // you see complete structure of aObject variable.
