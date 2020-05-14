695bae28698e8b178d0c6df9f0564ef89cb3778a
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-15 00:13:07 +0530

    updating a comment

--- app-object.js
+++ app-object.js
@@ -1,4 +1,4 @@
-// Object are key-value pairs and note reference type.
+// Object are key-value pairs and important-note: they reference type.
 const aObject = { id: 1, username: "sandeep" }; // here aObject is pointing to the address in the memory. where it stores the data.
 console.log(aObject);
 console.dir(aObject); // you see complete structure of aObject variable.
