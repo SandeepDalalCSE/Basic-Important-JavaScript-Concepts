dfaab781d6a0d37f515e78790c277613e4fce983
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-15 07:05:56 +0530

    adding and deleting key-value pair in an Object

--- app-object.js
+++ app-object.js
@@ -2,3 +2,19 @@
 const aObject = { id: 1, username: "sandeep" }; // here aObject is pointing to the address in the memory. where it stores the data.
 console.log(aObject);
 console.dir(aObject); // you see complete structure of aObject variable.
+
+// If we want to add more key-value, if we haven't added it in an Object before. say in aObject.
+// object.key = value
+aObject.userEmail = "sandeepdalal1995@gmail.com";
+aObject.age = 32;
+console.log(aObject);
+
+// If we want to delete any property/key-value pair
+delete aObject.userEmail; // userEmail key-value pair is deleted.
+console.log(aObject);
+
+// selectBtn;
+// console.dir(document.getElementById("selectBtn"));
+// const buttonAdded = document.getElementById("buttonAppendHere");
+// document.createElement("BUTTON").append("buttonAdded");
+// console.dir(document.createElement("button"));
